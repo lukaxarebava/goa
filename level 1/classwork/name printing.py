2e3d8b4b446b8b98cf1a02fe21fff40b8c8e6e2f
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-05-17 16:39:11 +0400

    first commit

---
+++ level 1/classwork/name printing.py
@@ -0,0 +1,12 @@
+name = "luka"
+
+surname = "xarebava"
+
+age = 13
+height = 150   #ჩემი სიმაღლე არ ვიცი, მაგრამ მიახლოებით 150 ვარ
+
+knows_programming = True
+is_ugly = False
+
+print(name + " " + surname)
+
---
+++ level 1/homework/info abt me.py
@@ -0,0 +1,8 @@
+name = "luka"
+
+surname = "xarebava"
+
+age = 13
+height = 150
+
+print("i am " + name + " " + surname + " " + str(age) + " years old " + "my height is  " +  str(height) + " cm")
