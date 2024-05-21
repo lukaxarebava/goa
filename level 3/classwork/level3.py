04a291471e5aaaff016235211629fcefe8646462
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-05-21 21:41:53 +0400

    level 3

---
+++ level 3/classwork/level3.py
@@ -0,0 +1,29 @@
+#print("hello world")
+#print(10)
+
+#name(luka)
+
+#last_name = "xarebava"
+
+#age = 13
+
+#print(name, last_name, age)
+
+
+
+
+
+
+
+#print(50 + 60) # + mimateba
+#print(60 - 50) # - gamokleba
+#print(60 - 150) 
+
+#print(25 * 25) # * gamravleba
+#print(100 / 2) # / atwiladi
+#print(100 // 2) # // gayofa
+
+name1 = "luka"
+
+print(name)
+
---
+++ level 3/homework/level 3 homework.py
@@ -0,0 +1,30 @@
+name = "luka"
+last_name = "xarebava"
+age = "13"
+country = "georgia"
+
+
+print(name, last_name, age, country)
+
+item = "yellow"
+item2 = "house"
+
+print(item + item2)
+
+
+
+
+
+
+print(50 + 50)
+print(284 + 481)
+print(58 - 8)
+print(785 - 375)
+print(5 * 8)
+print(74 * 84)
+print(150 / 2)
+print(573 / 3)
+print(650 // 5)
+print(248 // 4)
+print(6 ** 7)
+
