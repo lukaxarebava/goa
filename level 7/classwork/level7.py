d1540fbdb5746d648036f32a2b4e3777330e199c
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-06-05 01:03:51 +0400

    level 7

---
+++ level 7/classwork/level7.py
@@ -0,0 +1,35 @@
+#salary = 150000
+#pay_raise = 10000
+#new_salary = (salary + pay_raise)
+#print(new_salary)
+
+
+
+
+
+
+
+
+#name = input("what is your name: ")
+#last_name = input("what is your last_name: ")
+#age = input("what is your age: ")
+#country = input("where are you from: ")
+
+#print(name, last_name, age, country)
+
+
+
+
+
+
+
+
+#name = "gega"
+#age = 14
+#salary = 3.15
+
+#print(name, age, salary)
+
+#print(type(name))
+#print(type(age))
+#print(type(salary))
---
+++ level 7/homework/level 7.py
@@ -0,0 +1,22 @@
+#name = input("What is your name: ")
+#last_name = input("What is your last name: ")
+#age =  input("What is your age: ")
+#country =  input("Where are you from: ")
+#favourite_color = input("What is your favourite color: ")
+#favourite_car_brand = input("What is your favourite car brand: ")
+
+#print(name, last_name, age, country, favourite_color, favourite_car_brand )
+
+
+
+
+
+dialog_1 = input("hello what is your name: ")
+dialog_2 = input("hi my name is giorgi, what is your name: ")
+dialog_3 = input("my name is lazare, nice to meet you:")
+dialog_4 = input("lets be friends: ")
+dialog_5 = input("okay why not: ")
+dialog_6 = input("see you tommorow: ")
+dialog_7 = input("goodbye: ")
+
+print(dialog_1, dialog_2, dialog_3, dialog_4, dialog_5, dialog_6, dialog_7)
