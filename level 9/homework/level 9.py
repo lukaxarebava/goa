895fe4396c16cd60d025a1eb687b2a3f17c19941
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-06-12 20:49:09 +0400

    level 9

---
+++ level 9/homework/level 9.py
@@ -0,0 +1,29 @@
+#დაწერეთ პითონის პროგრამა, რომელიც მოუწოდებს მომხმარებელს შეიყვანოს ორი რიცხვი და შემდეგ დაბეჭდოს მათი ჯამი.
+
+someone = input("hello: ")
+
+print(someone)
+
+someone = input("plese  type 2 numbers: ")
+
+print(someone)
+
+someone = input("good, now add them to each other: ")
+
+print(someone)
+
+someone = input("now type their sum: ")
+
+print(someone)
+
+someone = input("are you sure its correct answer? ")
+
+print(someone)
+
+someone = input("okay, you just wasted your time :) ")
+
+print(someone)
+
+someone = input("bye see you in other dialogs: ")
+
+print(someone)
