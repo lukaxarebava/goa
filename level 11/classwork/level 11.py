decaa4bc7a84079fb3aa900db56e65762250c0c4
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-07-29 16:19:06 +0400

    commit

---
+++ level 11/classwork/level 11.py
@@ -0,0 +1,17 @@
+#savings = float(input("Enter your savings: "))
+
+#year_savings = savings * 1.05
+
+#overall_savings = str(year_savings)
+
+#print("your new savings are", overall_savings)
+
+
+
+
+#print(10 > 5)    #მეტია
+#print(1 < 5)     #ნაკლებია
+#print(10 >= 10)  #მეტია ან ტოლი
+#print(10 <= 10)  #ნაკლები ან ტოლი
+#print(10 == 10)  #ტოლია
+#print(10 != 10)  #არ არის ტოლი
---
+++ level 13/homework/level 13.py
@@ -0,0 +1,50 @@
+# 1. გააკეთეთ 10 მაგალითი ლოგიკურ ოპერატორებზე(True and False  - ამ გზით არა)
+
+item1 = 3
+item2 = 6
+item3 = 45
+item4 = 15
+
+#and
+
+print(item1 < item2 and item3 > item4)
+
+print(item1 > item4 and item2 > item3)
+
+print(item1 < item2 and item3 < item4)
+
+print(item1 > item4 and item3 < item2)
+
+print(item3 > item2 and item4 < item1)
+
+print(item2 > item1 and item3 > item4)
+
+print(item4 < item3 and item1 > item4)
+
+print(item3 <= item2 and item4 >= item1)
+
+print(item1 != item2 and item3 != item4)
+
+print(item1 <= item4 and item2 >= item3)
+
+#or
+
+print(item1 != item2 or item3 != item4)
+
+print(item2 > item4 or item1 < item3)
+
+print(item4 == item4 or item1 == item1)
+
+print(item3 <= item3 or item4 >= item1)
+
+print(item4 == item4 or item1 == item1)
+
+print(item1 > item4 or item3 < item2)
+
+print(item1 <= item4 or item2 >= item3)
+
+print(item1 < item2 or item3 > item4)
+
+print(item4 < item3 or item1 > item4)
+
+print(item3 > item1 or item4 < item2)
