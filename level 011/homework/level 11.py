9eba9dd0698c8141e11033e0762086d08c8080ed
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-10-29 19:13:33 +0400

    level 11

--- level 011/homework/level 11.py
+++ level 011/homework/level 11.py
@@ -1,52 +1,34 @@
-# 1) გააკეთეთ 30 მაგალითი შედარებით ოპერატორებზე. 
 
-print(200 > 1)
-print(1 > 200)
-print(485 > 205)
-print(836 < 1194)
-print(30 > 25)
-print(74 >= 74)
-print(430 <= 945)
-print(7495 <= 7495)
-print(8471 == 8471)
-print(7402 == 863)
-print(836 >= 437)
-print(1714 >= 8502)
-print(97481 == 981748)
-print(10 != 10)
-print(10 != 12)
-print(300 != 400)
-print(400 != 400)
-print(1094 != 8749)
-print(100 != 100)
-print(1060 > 940)
-print(4038 > 4037)
-print(1038 < 7000)
-print(1038 >= 7000)
-print(1047 < 9405)
-print(1084 < 699)
-print(1121 == 1121)
-print(4 > 1)
-print(6 > 1)
-print(8371 > 1)
-print(0 == 0)
-print(193748919 >= 10347842)
+# name = input("seiyvanet tqveni saxeli: ")
+# age = int(input("seiyvanet tqveni asaki: "))
+# height = float(input("seiyvanet tqveni simagle: "))
 
-# 2) დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი.
+# if age > 18 and name == "luka" and height > 1.80:
+#     print("piroba dakmafobilda.")
+# else:
+#     print("piroba ar dakmafobilda.")
 
-# ნაბიჯები:
 
-# შესთავაზეთ მომხმარებელს შეიყვანოს პირველი პირის ასაკი.
-# სთხოვეთ მომხმარებელს შეიყვანოს მეორე პირის ასაკი.
-# შეადარეთ ორი ასაკი შედარების ოპერატორების გამოყენებით.
-# გამოიტანეთ შეტყობინება, რომელიც მიუთითებს იმაზე, თუ ვინ არის უფროსი ან იმავე ასაკის.
 
-person1 = int(input("what is you age: "))
-person2 = int(input("what is you age: "))
 
-print(person1 > person2)
-print(person1 < person2)
-print(person1 >= person2)
-print(person1 <= person2)
-print(person1 == person2)
-print(person1 != person2)
+
+
+
+# or1 = True or False  
+# or2 = False or False  
+# or3 = True or True  
+# or4 = 5 > 3 or 2 < 1  
+
+
+# and1 = True and False  
+# and2 = True and True  
+# and3 = 5 < 10 and 3 > 1 
+# and4 = False and 1 == 1  
+
+
+
+
+
+# flowchart - არის ალგორითმის ახსნა ფოტოს მეშვეობით
+# pseudocode - გამოიყენება ალგორითმის მარტივარ აღსათქმელად
+# natural language - არის ალგორითმის გამოსახვა ბუნებრივი ენით
