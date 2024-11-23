1a93ce2f96288e854852a73da3be3e23ae1e2ca7
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-11-23 15:50:22 +0400

    level 23

---
+++ level 23/homework/homework.py
@@ -0,0 +1,36 @@
+# 1)
+
+# name = input("LUKA: ")
+
+# name = name.lower()
+
+# print(f"პატარა ასოებით: {name}")
+
+
+# 2)
+
+# surname = "xarebava"
+
+# surname_upper = surname.upper()
+
+# print(f"დიდი ასოებით: {surname_upper}")
+
+
+# 3)
+
+# text = "hello"
+
+# text_capitalized = text.capitalize()
+
+# print(f"შედეგი: {text_capitalized}")
+
+
+# 4)
+
+# words = ["gold", "luka", "car", "burger"]
+
+# uppercase_words = []
+# for word in words:
+#     uppercase_words.append(word.upper())
+
+# print("დიდ ასოებად:", uppercase_words)
