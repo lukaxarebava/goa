f6c4fa6f449af0e11df6aab1d7b5131410f0f74b
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-11-25 19:32:19 +0400

    level 17

---
+++ level 017/homework/homework.py
@@ -0,0 +1,43 @@
+# list = ["ვაშლი", "ბანანი", "ფორთოხალი", "ყურძენი", "ატამი"]
+
+# for index, item in enumerate(list):
+#     print(f"ინდექსი {index}: {item}")
+
+
+
+
+
+
+# my_list = ["ვაშლი", "ბანანი", "ფორთოხალი", "ყურძენი", "ატამი", 
+#            "ნესვი", "საზამთრო", "კივი", "ლიმონი", "მანგო"]
+
+# print("ელემენტების გამოყვანა range() ფუნქციით:")
+
+# for i in range(len(my_list)):
+#     print(f"ინდექსი {i}: {my_list[i]}")
+
+# print("\nელემენტების გამოყვანა ცვლადის გადაცემით:")
+
+# for item in my_list:
+#     print(f"ელემენტი: {item}")
+
+
+
+
+
+
+# numbers = [4, 12, 7, 15, 9, 25, 3, 18, 11, 5]
+
+# for number in numbers:
+#     if number > 10:
+#         print(number)
+
+
+
+
+
+
+# my_list2 = [42, "ტექსტი", 3.14, True, None, [1, 2, 3], {"სახელი": "bmw"}]
+
+# for item in my_list2:
+#     print(f"ელემენტი: {item}, ტიპი: {type(item)}")
