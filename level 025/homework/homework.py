d296149a902f125122f272f38a7197203281b13c
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-12-03 18:55:50 +0400

    level 25

---
+++ level 025/homework/homework.py
@@ -0,0 +1,46 @@
+# even_numbers = []
+# for i in range(201):
+#     if i % 2 == 0:
+#         even_numbers.append(i)
+
+# print(even_numbers)
+
+
+
+
+# favorite_names = []
+
+# for i in range(5):
+#     name = input(f"Enter your favorite name #{i+1}: ")
+#     favorite_names.append(name)
+
+# print("Your top 5 favorite names are:", favorite_names)
+
+
+
+
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for i in range(1, len(my_list), 2):
+#     print(my_list[i])
+
+
+
+# my_list = [10, 20, 30, 40, 50]
+
+# index = int(input("Enter a number between 0 and 4: "))
+
+
+# if 0 <= index < len(my_list):
+#     removed_element = my_list.pop(index)
+#     print(f"The removed element is: {removed_element}")
+# else:
+#     print("Invalid Please enter a number between 0 and 4.")
+
+
+
+
+
+# my_list = [1, 2, 3, 4, 5]
+# print("The length of the list is:", len(my_list))
