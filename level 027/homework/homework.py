9e115b86f9f5c47006195dfc1a41eed55198b39a
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-12-11 19:57:19 +0400

    level 27

---
+++ level 027/homework/homework.py
@@ -0,0 +1,47 @@
+# def sum_two_numbers(a: float, b: float) -> float:
+#     return a + b
+
+# try:
+#     number1 = float(input("Enter the first number: "))
+#     number2 = float(input("Enter the second number: "))
+#     result = sum_two_numbers(number1, number2)
+#     print(f"The sum of {number1} and {number2} is {result}")
+# except ValueError:
+#     print("Invalid input Please enter numbers.")
+
+
+
+
+
+# def even_or_odd(number):
+#     return "Even" if number % 2 == 0 else "Odd"
+
+# try:
+#     num = int(input("Enter a number: "))
+#     result = even_or_odd(num)
+#     print(f"The number {num} is {result}.")
+# except ValueError:
+#     print("Invalid input. Please enter numbers.")
+
+
+
+
+
+
+
+
+
+# def number_sign(num):
+#     if num > 0:
+#         return "დადებითი"
+#     elif num < 0:
+#         return "უარყოფითი"
+#     else:
+#         return "ნული"
+
+# try:
+#     user_input = float(input("შეიყვანეთ რიცხვი: "))
+#     print(f"რიცხვი {user_input} არის {number_sign(user_input)}")
+# except ValueError:
+#     print("შეყვანეთ რიცხვი")
+
