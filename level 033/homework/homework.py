8b45b6d0e0b6c7bae93a5d70b5b63cb3d8a63350
lukaxarebava <lukaprogamerxarebava@gmail.com>
2024-12-30 12:29:55 +0400

    level 32 & 33

---
+++ level 033/homework/homework.py
@@ -0,0 +1,37 @@
+# my_list = ["ელემენტი 1", "ელემენტი 2", "ელემენტი 3", "ელემენტი 4", "ელემენტი 5"]
+
+# del my_list[2]
+
+# my_list.insert(0, "ახალი ელემენტი")
+
+# print(my_list)
+
+
+
+
+
+# def raise_to_power(base, exponent):
+#     return base ** exponent
+
+# result = raise_to_power(2, 3) 
+# print(result)  
+
+
+
+
+# def check_list_length(my_list):
+    
+#     if len(my_list) % 2 == 0:
+#         return "List length is even"
+#     else:
+#         return "List length is odd"
+
+
+# result1 = check_list_length([1, 2, 3, 4])  
+# result2 = check_list_length([1, 2, 3])  
+
+# print(result1)  
+# print(result2)  
+
+
+
