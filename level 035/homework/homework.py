70f71da8b9a87dfa835e9b42f28d95e0ce9ec8a4
lukaxarebava <lukaprogamerxarebava@gmail.com>
2025-01-10 18:10:14 +0400

    level 35

---
+++ level 035/homework/homework.py
@@ -0,0 +1,14 @@
+# def manual_join(separator, elements):  
+#     if not elements:
+#         return ""
+    
+#     result = ""
+#     for i, element in enumerate(elements):
+#         result += str(element)
+#         if i < len(elements) - 1:
+#             result += separator
+#     return result
+
+# my_list = ["apple", "banana", "cherry"]
+# result = manual_join(", ", my_list)
+# print(result) 
