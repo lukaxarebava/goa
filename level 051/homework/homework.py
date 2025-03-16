8e3ceadf9772553961cc66d8185ecfca44ef6afc
lukaxarebava <lukaprogamerxarebava@gmail.com>
2025-03-17 01:46:48 +0400

    second commit

---
+++ level 051/homework/homework.py
@@ -0,0 +1,42 @@
+# shopping_list = ("პური", "რძე", "კვერცხი", "ვაშლი")
+
+# bread, milk, eggs, apple = shopping_list
+
+# print("პური:", bread)
+# print("რძე:", milk)
+# print("კვერცხი:", eggs)
+# print("ვაშლი:", apple)
+
+
+
+
+
+
+
+
+# fast_food = ("ბურგერი", "ფრი", "პიცა")
+
+# healthy_food = list(fast_food)
+# healthy_food.extend(["ვაშლი", "ბროკოლი", "ავოკადო"])
+
+# updated_food = tuple(healthy_food)
+
+# print(updated_food)
+
+
+
+
+
+
+
+
+# Asterisk (*) არის სიმბოლო, რომელსაც Python-ში სხვადასხვა ფუნქცია აქვს
+
+# 1. Unpacking
+# გამოიყენება სიის (list), tuple-ის ან სხვა iterable-ის გაშლისთვის და ცვლადებში მოსათავსებლად.
+
+# 2. Multiple Values Capture
+# * საშუალებას გაძლევთ შეინახოთ მრავალი ელემენტი ერთ ცვლადში.
+
+# 3. Function Arguments
+# გამოიყენება ფუნქციაში რამდენიმე არგუმენტის მისაღებად ან გადასაცემად.
