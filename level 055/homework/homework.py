e7b79cbc9bc62cd2c806f717763fb1579e5c228b
lukaxarebava <lukaprogamerxarebava@gmail.com>
2025-03-17 17:01:39 +0400

    second commit

---
+++ level 055/homework/homework.py
@@ -0,0 +1,39 @@
+# info = {
+#     "სახელი": "ლუკა",
+#     "ასაკი": 14,
+#     "ქალაქი": "თბილისი"
+# }
+
+# keys_list = []
+# values_list = []
+
+# for key, value in info.items():
+#     keys_list.append(key)
+#     values_list.append(value)
+
+# print("Keys:", keys_list)
+# print("Values:", values_list)
+
+
+
+# def remove_duplicates(lst):
+#     result = []
+#     for item in lst:
+#         if item not in result:
+#             result.append(item)
+#     return result
+
+# my_list = [1, 2, 2, 3, 4, 4, 5]
+# unique_list = remove_duplicates(my_list)
+# print(unique_list)
+
+
+
+
+
+
+# result = [10, 43, 12, 11, 94, 10, 55, 7, 11]
+
+# result = list(dict.fromkeys(result))
+
+# print(result)
